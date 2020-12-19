c02dd4ca51a0a5545b6b8b81e1437de4d338c511
Jesper Jensen <jeesondk@outlook.com>
2020-12-19 16:01:18 +0100

    Inital commit

---
+++ CustomCollection/ExtededIntegerArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace CustomCollection
+{
+    public class ExtededIntegerArray
+    {
+
+        private int[] sum;
+        private int[] values;
+
+        public ExtededIntegerArray(int size)
+        {
+            if (size < 1)
+                throw new ArgumentOutOfRangeException("Array size must be greater than 1");
+
+            sum = new int[size];
+            values = new int[size];
+        }
+
+        public int this[int index] { get => values[index]; set => SetValue(value, index); }
+
+        public int Length => values.Length;
+
+        public int SumOf(int position1, int position2) => sum[position2] - sum[position1];
+
+
+        private void SetValue(int value, int index)
+        {
+            values.SetValue(value, index);
+            if (index > 0)
+                sum[index] = value + sum[index - 1];
+            else
+                sum[index] = value;
+        }
+    }
+}
---
+++ CustomCollectionTests/ExtendedInterArrayTests.cs
@@ -0,0 +1,71 @@
+using CustomCollection;
+using NUnit.Framework;
+using System;
+
+namespace CustomCollectionTests
+{
+    public class ExtendesIntergerArrayTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CanStoreIntegers()
+        {
+            var array = new ExtededIntegerArray(1);
+            Assert.DoesNotThrow(() => array[0] = 1);
+        }
+        
+        [Test]
+        public void ThrowsOnZeroLenght()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ExtededIntegerArray(0));
+        }
+
+        [Test]
+        public void ShouldHavePositionForElements()
+        {
+            var array = new ExtededIntegerArray(10);
+            array[8] = 256;
+            Assert.AreEqual(256, array[8]);
+        }
+
+        [Test]
+        public void ShouldBeZeroIndexed()
+        {
+            var array = new ExtededIntegerArray(10);
+            array[0] = 1;
+            Assert.AreEqual(1, array[0]);
+        }
+
+        [Test]
+        public void ShouldHaveLastPositionAsLenghtMinusOne()
+        {
+            var array = new ExtededIntegerArray(10);
+            array[array.Length - 1] = 10;
+            Assert.AreEqual(10, array[array.Length - 1]);
+        }
+
+        [Test]
+        public void ShouldthrowWhenAccesingPostionEqualToLenght()
+        {
+            var array = new ExtededIntegerArray(10);
+            Assert.Throws<IndexOutOfRangeException>(() => array[array.Length] = 11);
+        }
+
+        [Test]
+        public void CanCalculateSumBetweenTwoPositions()
+        {
+            var array = new ExtededIntegerArray(100);
+
+            for(int i = 0; i < 100; i++)
+            {
+                array[i] = i+1;
+            }
+
+            Assert.AreEqual(1010, array.SumOf(39, 59));
+        }
+    }
+}
